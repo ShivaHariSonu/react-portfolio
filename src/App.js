00330fef3f6338c143af7dd24b3d488a6d3a4043
shivahari <shivaharigundeti@gmail.com>
2024-01-03 17:28:43 -0700

    testingone

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path = '' element = {<Layout/>}>
-        <Route index element = {<Home/>}/> 
-        <Route path='about' element = {<About/>}/> 
-        <Route path='contact' element = {<Contact/>}/> 
+      <Route path = '/' element = {<Layout/>}>
+        <Route path = '/' element = {<Home/>}/> 
+        <Route path='/about' element = {<About/>}/> 
+        <Route path='/contact' element = {<Contact/>}/> 
       </Route>
     </Routes>
     </>
